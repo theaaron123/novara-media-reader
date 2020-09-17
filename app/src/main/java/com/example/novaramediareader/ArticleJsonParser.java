207e465b69061bc73b5ab36903cf76325f61c43d
Aaron Baker <aaronjsunk@gmail.com>
2020-09-17 10:32:04 +0200

    Refactor json processing into helper class from MainActivity

---
+++ app/src/main/java/com/example/novaramediareader/ArticleJsonParser.java
@@ -0,0 +1,57 @@
+package com.example.novaramediareader;
+
+import android.text.Html;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArticleJsonParser {
+    static List<Articles> addItemsFromJSON(InputStream inputStream) {
+        try {
+            String jsonDataString = readJSONDataFromFile(inputStream);
+            JSONObject object = new JSONObject(jsonDataString);
+            JSONArray jsonArray = object.getJSONArray("posts");
+            List<Articles> articlesList = new ArrayList<>();
+
+            for (int i = 0; i < jsonArray.length(); ++i) {
+                JSONObject itemObj = jsonArray.getJSONObject(i);
+
+                String name = itemObj.getString("title");
+                String shortDesc = itemObj.getString("short_desc");
+
+                articlesList.add(new Articles(name, Html.fromHtml(shortDesc).toString().replaceAll("\n", "").trim()));
+            }
+            return articlesList;
+        } catch (JSONException | IOException e) {
+            Log.d(MainActivity.class.getName(), "addItemsFromJSON: ", e);
+        }
+        return null;
+    }
+
+    private static String readJSONDataFromFile(InputStream inputStream) throws IOException {
+        StringBuilder builder = new StringBuilder();
+
+        try {
+            String jsonString;
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"));
+
+            while ((jsonString = bufferedReader.readLine()) != null) {
+                builder.append(jsonString);
+            }
+        } finally {
+            if (inputStream != null) {
+                inputStream.close();
+            }
+        }
+        return new String(builder);
+    }
+}
